681a50a9ffe951589f85bc00f774af3341b17971
siratul <asatulsingh20@gmail.com>
2020-07-07 16:30:28 +0530

    Updates

---
+++ precache-manifest.824b938512d0b99d0168e6f755f306c2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8e8a3d4d226873280d6da5d7c01b839",
+    "url": "/Bill-Splitter/index.html"
+  },
+  {
+    "revision": "7b79af0370c1e389a3ee",
+    "url": "/Bill-Splitter/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "abb32afefb346682c24e",
+    "url": "/Bill-Splitter/static/css/main.7bd109d0.chunk.css"
+  },
+  {
+    "revision": "7b79af0370c1e389a3ee",
+    "url": "/Bill-Splitter/static/js/2.de0f8e8b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Bill-Splitter/static/js/2.de0f8e8b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "abb32afefb346682c24e",
+    "url": "/Bill-Splitter/static/js/main.dce0dbd6.chunk.js"
+  },
+  {
+    "revision": "9b371a634fd0380b8062",
+    "url": "/Bill-Splitter/static/js/runtime-main.205b8b97.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/Bill-Splitter/precache-manifest.824b938512d0b99d0168e6f755f306c2.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Bill-Splitter/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
